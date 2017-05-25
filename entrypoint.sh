8ce788c4b139e9657a625014d9d2d64c6b80cabb
Sheogorath <sheogorath@shivering-isles.com>
2017-05-25 18:38:45 +0200

    Run in foreground and fix socket error

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,10 @@ freshclam
 
 sed -i 's/#TCPAddr 127.0.0.1/TCPAddr 0.0.0.0/g' /etc/clamav/clamd.conf
 sed -i 's/#TCPSocket 3310/TCPSocket 3310/g' /etc/clamav/clamd.conf
+sed -i 's/PidFile /run/clamav/clamd.pid/#PidFile /run/clamav/clamd.pid/g' /etc/clamav/clamd.conf
+sed -i 's/#Foreground yes/Foreground yes/g' /etc/clamav/clamd.conf
+
+mkdir /run/clamav
+chown clamav:clamav /run/clamav/
 
 clamd $@
